6d390d36eceac0e930306d85a782b13b38cefe03
vpoiskaxalaski <31599181+vpoiskaxalaski@users.noreply.github.com>
2019-10-26 22:38:10 +0300

    Interacting with objects on stage

---
+++ lab11/Assets/forCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forCamera : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float senseHor = 9.0f;
+    public float senseVer = 9.0f;
+    public float MaxVer = 45.0f;
+    public float MinVer = -45.0f;
+    private float _rotationX = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * senseHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * senseVer;
+            _rotationX = Mathf.Clamp(_rotationX, MinVer, MaxVer);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f);
+        }
+    }
+}
---
+++ lab11/Assets/forCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forCube : MonoBehaviour
+{
+    int count;
+    public Color[] colors = { Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.grey, Color.magenta, Color.red, Color.yellow, Color.white };
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = colors.Length - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        if(count < 0)
+        {
+            count = colors.Length-1;
+        }
+        
+        gameObject.GetComponent<Renderer>().material.color = colors[count];
+        gameObject.GetComponent<Rigidbody>().AddForce(0, 300, 0);
+        count = count - 1;
+
+    }
+
+}
